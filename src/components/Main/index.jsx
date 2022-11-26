8ced35e2cbf32bdd344708ca7bcd8b88cd651b67
Nariman <n.murzakaev@big3.ru>
2022-11-26 08:01:33 +0300

    i have learned context API, useReducer and localStorage, sooooo....

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -8,13 +8,12 @@ import Search from "../Search";
 import "./index.scss";
 
 export default function Main({sneakers, isSneakerAdded, setIsSneakerAdded}) {
-
   const {searchSneaker, setSearchSneaker} = useContext(ContextSearchSneaker);
   const {cartSneakers, setCartSneakers} = useContext(ContextCartSneakers);
-
   const searchByField = 'descr';
-
   const [searchedSneaker, setSearchedSneaker] = useState([]);
+  const renderItems = searchedSneaker.map((s, idx) => <Card key={idx} sneaker={s} isSneakerAdded={isSneakerAdded} setIsSneakerAdded={setIsSneakerAdded}/>);
+  const searchText = searchSneaker ? `Searching by: ${searchSneaker}` : 'All Sneakers';
 
   useEffect(() => {
     axios.get('https://635a769c6f97ae73a62d5445.mockapi.io/cart').then(res => setCartSneakers(res.data));
@@ -23,10 +22,6 @@ export default function Main({sneakers, isSneakerAdded, setIsSneakerAdded}) {
       return s[searchByField].match(searchSneaker, 'gi');
     }) || sneakers);
   }, [sneakers, searchSneaker]);
-
-  const renderItems = searchedSneaker.map((s, idx) => <Card key={idx} sneaker={s} isSneakerAdded={isSneakerAdded} setIsSneakerAdded={setIsSneakerAdded}/>);
-
-  const searchText = searchSneaker ? `Searching by: ${searchSneaker}` : 'All Sneakers';
 
   return <main className="main">
     <div className="title__container">
